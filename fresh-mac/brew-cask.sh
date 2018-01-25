1628c76db5ed23522086abbe8b09663b9c43e085
Victor Sollerhed <victor.sollerhed@gmail.com>
2018-01-25 15:40:06 +0100

    Added brew cask for fresh Mac install.

---
+++ fresh-mac/brew-cask.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+#------- Example from recent install:
+# $ brew cask list -1
+# 1password
+# aerial
+# alfred
+# atom
+# bowtie
+# ccmenu
+# discord
+# docker
+# dropbox
+# fluid
+# google-backup-and-sync
+# google-cloud-sdk
+# intellij-idea-ce
+# iterm2
+# java8
+# macdown
+# messenger
+# minikube
+# phpstorm
+# rescuetime
+# rocket
+# slack
+# spotify
+# typora
+# virtualbox
+# visual-studio-code
+# yakyak
+
+brew cask install 1password
+brew cask install aerial											# Apple TV screensavers
+brew cask install alfred
+brew cask install discord
+brew cask install dropbox
+brew cask install docker
+brew cask install google-backup-and-sync			# Google Drive sync
+brew cask install iterm2
+brew cask install messenger
+brew cask install rocket
+brew cask install spotify
+brew cask install virtualbox
