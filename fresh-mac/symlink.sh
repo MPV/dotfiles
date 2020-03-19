9675aa34835eb83caf05b31dcc3fcde1796c87b0
Victor Sollerhed <victor.sollerhed@pagero.com>
2020-03-19 13:26:42 +0100

    symlink ~/.zshrc

--- fresh-mac/symlink.sh
+++ fresh-mac/symlink.sh
@@ -12,6 +12,7 @@ ln -s ${PARENT_PATH}/.gitignore
 ln -s ${PARENT_PATH}/.skhdrc
 ln -s ${PARENT_PATH}/.vimrc
 ln -s ${PARENT_PATH}/.yabairc
+ln -s ${PARENT_PATH}/.zshrc
 ln -s ${PARENT_PATH}/.config/starship.toml ~/.config/
 ln -s ${PARENT_PATH}/.ssh/personal.include ~/.ssh/
 #ln -s ${PARENT_PATH}/Brewfile
